c9dc8bbce99710d4d7dbf55525c4b63bd956571f
RP <rogerpino94@gmail.com>
2021-09-02 15:58:28 -0500

    Starting project

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../assets/styles/App.scss";
 
-const HelloWorld = () => <h1>Hola mundo rrrrrico</h1>;
+const HelloWorld = () => <h1>Hola mundo</h1>;
 
 export default HelloWorld;
